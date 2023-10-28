ada72c4f5701b14945dfa293afa0b54b34f62541
KorneliuszSwierczek <korneliusz.swierczek@microsoft.wsei.edu.pl>
2023-10-28 10:42:41 +0200

    Dodaj pliki projektów.

---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using HotelReservationSystem.Models;
+
+namespace HotelReservationSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private static List<Reservation> reservations = new List<Reservation>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Reservation>> Get()
+        {
+            return Ok(reservations);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Reservation reservation)
+        {
+            reservation.Id = Guid.NewGuid().GetHashCode();
+            reservations.Add(reservation);
+            return CreatedAtAction(nameof(Get), new { id = reservation.Id }, reservation);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var reservation = reservations.Find(r => r.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            reservations.Remove(reservation);
+            return NoContent();
+        }
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HotelReservationSystem.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; }
+        public string LastName { get; set; } 
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public int RoomNumber { get; set; }
+    }
+}
